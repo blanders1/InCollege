e6d3cbacb48ebd2b0a6d2b42bf9ca9e20f52c406
Ben Landers <blanders1@usf.edu>
2023-06-10 14:25:11 -0400

    implemented search function.

--- Pages/ConnectionsPage.py
+++ Pages/ConnectionsPage.py
@@ -6,20 +6,21 @@ import MainMenu as menu
 class ConnectionsPage:
     @staticmethod
     def load_connections():
-        print("Would you like to find a connection with someone you know?")
+        print("\nWould you like to find a connection with someone you know?")
         print("1.) Yes ")
         print("2.) No return to previous")
 
-        choice = int(input("Please enter 1 or 2: "))
-        print("\n")
+        choice = int(input("\nPlease enter 1 or 2: "))
         if choice == 1:
-            first_name = input("Please enter their first name: ")
+            first_name = input("\nPlease enter their first name: ")
             last_name = input("Please enter their last name: ")
             user = db.check_name(first_name.lower(), last_name.lower())
             if user:
-                print("They are part of the InCollege system\n")
+                print("\nThey are part of the InCollege system\n")
+                ConnectionsPage.load_connections()
             else:
-                print("They are not yet a part of the InCollege system yet\n")
+                print("\nThey are not yet a part of the InCollege system yet\n")
+                ConnectionsPage.load_connections()
         
         elif choice == 2:
             return menu.MainMenu.main_menu_options
